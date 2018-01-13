29023edc18c8294fca5b91ef03cad2eae4d3eee5
rgabeflores <rgabeflores@gmail.com>
2018-01-13 14:05:23 -0800

    Migrated API to .csv Input

--- stock_scrape.py
+++ stock_scrape.py
@@ -1,13 +1,23 @@
 from urllib.request import urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup
+from contextlib import contextmanager
 import re
 from time import sleep
+import psutil
+import os
 
 import ux
 import statistics as stats
 
+def memory_usage():
+    process = psutil.Process(os.getpid())
+    mem = process.memory_info()[0] / float(2 ** 20)
+    return mem
+
 '''
+	Quickly track a portfolio.
+
 	List Pages ex.
 	https://finance.yahoo.com/world-indices
 
@@ -27,8 +37,8 @@ import statistics as stats
 '''
 
 MAIN_OPTIONS = (
-	"Enter a Symbol for a Quote",
-	"Get Quotes via CSV File Input"
+	"View Portfolio",
+	"Add Stock Symbol to Portfolio"
 )
 
 BASE_URL = "https://finance.yahoo.com/quote/"
@@ -40,49 +50,59 @@ def get_url(param=None):
 		param = quote(param)
 	return param + "?p=" + param
 
+@contextmanager
 def retrieve(url):
-
 	http_response = urlopen(url).read()
 	soup = BeautifulSoup(http_response, 'html.parser')
-
 	content = soup.find('div', {'id': 'quote-summary'})
-	
-	#	First Column -------------------------
-	p_close = content.find('td', {'data-test': "PREV_CLOSE-value"})
-	open_value = content.find('td', {'data-test': "OPEN-value"})
-	bid = content.find('td', {'data-test': "BID-value"})
-	ask = content.find('td', {'data-test': "ASK-value"})
-	days_range = content.find('td', {'data-test': "DAYS_RANGE-value"})
-	ftw_range = content.find('td', {'data-test': "FIFTY_TWO_WK_RANGE-value"})
-	volume = content.find('td', {'data-test': "TD_VOLUME-value"})
-	avg_volume = content.find('td', {'data-test': "AVERAGE_VOLUME_3MONTH-value"})
-	#	Second Column ------------------------
-	m_cap = content.find('td', {'data-test': "MARKET_CAP-value"})
-	beta = content.find('td', {'data-test': "BETA-value"})
-	pe_ratio = content.find('td', {'data-test': "PE_RATIO-value"})
-	eps = content.find('td', {'data-test': "EPS_RATIO-value"})
-	earn_date = content.find('td', {'data-test': "EARNINGS_DATE-value"})
-	f_div_yield = content.find('td', {'data-test': "DIVIDEND_AND_YIELD-value"})
-	ex_div = content.find('td', {'data-test': "EXDIVIDEND_DATE-value"})
-	y_targ_est = content.find('td', {'data-test': "ONE_YEAR_TARGET_PRICE-value"})
+	yield soup
 
-	return {
-		"Previous Close": p_close,
-		"Open": open_value,
-		"Bid": bid,
-		"Days Range": days_range,
-		"52 Week Range": ftw_range,
-		"Volume": volume,
-		"Average Volume": avg_volume,
-		"Market Cap": m_cap,
-		"Beta": beta,
-		"PE Ratio (TTM)": pe_ratio,
-		"EPS (TTM)": eps,
-		"Earnings Date": earn_date,
-		"Forward Dividend & Yield": f_div_yield,
-		"Ex-Dividend Date": ex_div,
-		"1Y Target Est": y_targ_est
-	}
+@contextmanager
+def scrape(url):
+	'''
+		TO-DO:
+		• Scrape Labels to get keys
+		• Use Generator
+	'''
+	with retrieve(url) as content: 
+		yield {
+				"Previous Close": content.find('td', {'data-test': "PREV_CLOSE-value"}),
+				"Open": content.find('td', {'data-test': "OPEN-value"}),
+				"Bid": content.find('td', {'data-test': "BID-value"}),
+				"Ask": content.find('td', {'data-test': "ASK-value"}),
+				"Days Range": content.find('td', {'data-test': "DAYS_RANGE-value"}),
+				"52 Week Range": content.find('td', {'data-test': "FIFTY_TWO_WK_RANGE-value"}),
+				"Volume": content.find('td', {'data-test': "TD_VOLUME-value"}),
+				"Average Volume": content.find('td', {'data-test': "AVERAGE_VOLUME_3MONTH-value"}),
+				"Market Cap": content.find('td', {'data-test': "MARKET_CAP-value"}),
+				"Beta": content.find('td', {'data-test': "BETA-value"}),
+				"PE Ratio (TTM)": content.find('td', {'data-test': "PE_RATIO-value"}),
+				"EPS (TTM)": content.find('td', {'data-test': "EPS_RATIO-value"}),
+				"Earnings Date": content.find('td', {'data-test': "EARNINGS_DATE-value"}),
+				"Forward Dividend & Yield": content.find('td', {'data-test': "DIVIDEND_AND_YIELD-value"}),
+				"Ex-Dividend Date": content.find('td', {'data-test': "EXDIVIDEND_DATE-value"}),
+				"1Y Target Est": content.find('td', {'data-test': "ONE_YEAR_TARGET_PRICE-value"})
+			}
+
+def csv_input(filename=None):
+	if filename is None:
+		filename = input("\n\tEnter the name of the file for input: ")
+
+	file_lines = []
+		
+	try:
+		with open(filename, "r") as f:
+			file_lines = f.readlines()
+	except FileNotFoundError as e:
+		print("File not found.")
+
+	symbols = (x.strip().split(",") for x in file_lines)
+
+	for x in symbols:
+		for y in x:
+			with scrape(BASE_URL + get_url(y.strip())) as data:
+				display(data)
+				sleep(1)
 
 def display(content):
 	print("\n\n\t\t" + ("____" * 10))
@@ -99,43 +119,19 @@ def display(content):
 		sleep(0.05)
 
 	print("\t\t" + ("____" * 10))
-	print("\t\t" + ("____" * 10) + "\n\n")
-
-def cli_input():
-	url = BASE_URL + get_url()
-
-	rows = retrieve(url)
-
-	display(rows)
-
-def csv_input(filename=None):
-	if filename is None:
-		filename = input("Enter the name of the file for input: ")
-	try:
-		with open(file, "r") as f_obj:
-			file_lines = f_obj.readlines()
-	except IOError as e:
-		print("File not found.")
-	
-
-				
+	print("\t\t" + ("____" * 10) + "\n\n")				
 
 def main():
 
-	cont = True
-	input_filename = "input.txt"
-
-	while cont:
+	def main_loop():
 		choice = ux.get_user_choice(options=MAIN_OPTIONS)
 
 		if choice == 1:
-			cli_input()
+			csv_input()
 		else:
-			csv_input(input_filename)
+			csv_input()
 		
-
-		cont = ux.to_continue()
-	
+	cont = ux.to_continue(main_loop)
 
 if __name__ == "__main__":
 	main()
