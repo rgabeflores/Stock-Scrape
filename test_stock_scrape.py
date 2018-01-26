a5fbab9ca4b39406cfaf8362ed12e8f5607eb1f3
rgabeflores <rgabeflores@gmail.com>
2018-01-26 11:16:25 -0800

    Added Test Module

---
+++ test_stock_scrape.py
@@ -0,0 +1,43 @@
+import unittest
+import logging
+import stock_scrape
+
+logging.basicConfig(filename='stock_scrape.log', level=logging.WARNING)
+
+
+class TestStockScrape(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('\n\tRunning Macro Setup...\n\n')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('\n\tRunning Macro Tear Down...\n')
+
+    def setUp(self):
+        print('\nRunning Micro Setup...\n')
+
+    def tearDown(self):
+        print('\nRunning Micro Tear Down...\n\n')
+
+    def test_get_url(self):
+        print('Testing get_url(func, sym)...')
+        expected = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=KD5IR7D86O9BCZYI"
+        result = stock_scrape.get_url("TIME_SERIES_DAILY", "MSFT")
+        self.assertEqual(result, expected)
+
+    def test_fswitch(self):
+        print('Testing fswitch(x)...')
+
+        expected = "TIME_SERIES_INTRADAY"
+        result = stock_scrape.fswitch(1)
+        self.assertEqual(result, expected)
+
+        expected = "TIME_SERIES_DAILY"
+        result = stock_scrape.fswitch(2)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
