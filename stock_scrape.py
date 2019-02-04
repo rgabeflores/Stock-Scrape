c61217a904716f2f83035cb6cafe6925f93b67e8
rgabeflores <rgabeflores@gmail.com>
2019-02-04 01:30:02 -0800

    minor changes

--- stock_scrape.py
+++ stock_scrape.py
@@ -5,7 +5,8 @@ from json import loads, JSONDecodeError
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 from datetime import datetime
-from modules import ux
+import numpy as np
+import pandas as pd
 import sys
 import logging
 
@@ -22,7 +23,6 @@ def fswitch(x):
 
 
 def get_url(func, sym):
-    # LOG FILE IO FOR API KEY
     try:
         with open("../key.txt", 'r') as f:
             API_KEY = f.readline().strip()
@@ -32,7 +32,8 @@ def get_url(func, sym):
         logging.critical("key.txt not found.")
         sys.exit()
     else:
-        parameters = "function={}&interval=5min&symbol={}&apikey={}".format(func, quote(sym), API_KEY)
+        # interval parameter Hard Coded for TIME_SERIES_INTRADAY API
+        parameters = "function={}&symbol={}&apikey={}".format(func, quote(sym), API_KEY)
         return BASE_URL + parameters
 
 
@@ -43,44 +44,54 @@ def retrieve(url):
     except JSONDecodeError as e:
         print('Data returned did not have valid JSON formatting.')
         sys.exit()
+    except Exception as e:
+        print(e)
     else:
         return json_data[list(json_data.keys())[1]]
 
 
-def parse_json(data):
+def build_frame(data):
+    '''
+        Returns a pandas.DataFrame object with OPEN,HIGH,LOW,CLOSE,VOLUME columns/attributes.
+    '''
     dates = []
-    m_open = []
-    m_high = []
-    m_low = []
-    m_close = []
-    m_volume = []
+    rows = []
+
     for date, info in data.items():
         # Intraday API Date Format: %Y-%m-%d %H:%M:%S
-        dates.append(datetime.strptime(date, '%Y-%m-%d %H:%M:%S'))
-        m_open.append(float(info['1. open']))
-        m_high.append(float(info['2. high']))
-        m_low.append(float(info['3. low']))
-        m_close.append(float(info['4. close']))
-        m_volume.append(float(info['5. volume']))
-
-    return (dates, m_open, m_high, m_low, m_close, m_volume)
+        dates.append(datetime.strptime(date, '%Y-%m-%d'))
+        rows.append(np.array([
+            float(info['1. open']),
+            float(info['2. high']),
+            float(info['3. low']),
+            float(info['4. close']),
+            # float(info['5. volume'])
+        ]))
+    return pd.DataFrame(rows, index=dates, columns=['OPEN', 'HIGH', 'LOW', 'CLOSE'])
 
 
 def main():
-    content = retrieve(get_url(fswitch(1), "MSFT"))
-    results = parse_json(content)
-    dates = mdates.date2num(results[0])
+    symbol = 'MSFT'
+    content = retrieve(get_url(fswitch(2), symbol))
+    df = build_frame(content)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    plt.setp(ax.get_xticklabels(), rotation=15)
+    # X-Axis Date Display Configurations
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # plt.setp(ax.get_xticklabels(), rotation=15)
 
-    for i in range(1, len(results) - 1):
-        plt.plot_date(dates, results[i], '-')
+    # for i in range(1, len(results) - 1):
+    #     plt.plot_date(dates, results[i], '-')
+    # print(df.describe())
+    # plt.plot(df)
 
+    for key in df:
+        plt.plot(df[key], label=key)
+    plt.title(symbol)
     plt.xlabel('Dates')
-    plt.ylabel('values')
-    # plt.legend()
+    plt.ylabel('Values')
+    plt.grid(True, color='k', linestyle='-')
+    plt.legend()
     plt.show()
 
 
